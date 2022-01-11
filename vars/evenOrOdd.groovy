5923c8e763f8691cea5d5ec420bcb866ac6ea815
dylanmeh <dylan.mehmedovic@concanon.com>
2022-01-11 12:57:05 -0500

    adding declarative pipeline from jenkins docs

---
+++ vars/evenOrOdd.groovy
@@ -0,0 +1,25 @@
+def call(int buildnumber) {
+    if (buildNumber % 2 == 0) {
+        pipeline {
+            agent any
+            stages {
+                stage('Even stage') {
+                    steps {
+                        echo "The build number is even"
+                    }
+                }
+            }
+        }   else {
+            pipeline {
+                agent any
+                stages {
+                    stage('Odd stage') {
+                        steps {
+                            echo "The build number is odd"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
